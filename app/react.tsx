6bbc8d3a115691d6d650646b32885f7a56bd36a9
tacosontitan <jkdavis1691@gmail.com>
2022-02-08 11:10:48 -0600

    Fixed classname attribute issue.

--- app/react.tsx
+++ app/react.tsx
@@ -1,42 +1,42 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-const loader = () => {
+const Loader = () => {
     return <div>
         <h1>AUTOMATE</h1>
-        <h3 class="span loader-message">
-            <span class='m'>A</span>
-            <span class='m'>&nbsp;</span>
-            <span class='m'>N</span>
-            <span class='m'>E</span>
-            <span class='m'>W</span>
-            <span class='m'>&nbsp;</span>
-            <span class='m'>H</span>
-            <span class='m'>O</span>
-            <span class='m'>P</span>
-            <span class='m'>E</span>
-            <span class='m'>&nbsp;</span>
-            <span class='m'>F</span>
-            <span class='m'>O</span>
-            <span class='m'>R</span>
-            <span class='m'>&nbsp;</span>
-            <span class='m'>S</span>
-            <span class='m'>O</span>
-            <span class='m'>R</span>
-            <span class='m'>C</span>
-            <span class='m'>E</span>
-            <span class='m'>&nbsp;</span>
-            <span class='m'>C</span>
-            <span class='m'>O</span>
-            <span class='m'>N</span>
-            <span class='m'>T</span>
-            <span class='m'>R</span>
-            <span class='m'>O</span>
-            <span class='m'>L</span>
+        <h3 className="span loader-message">
+            <span className='m'>A</span>
+            <span className='m'>&nbsp;</span>
+            <span className='m'>N</span>
+            <span className='m'>E</span>
+            <span className='m'>W</span>
+            <span className='m'>&nbsp;</span>
+            <span className='m'>H</span>
+            <span className='m'>O</span>
+            <span className='m'>P</span>
+            <span className='m'>E</span>
+            <span className='m'>&nbsp;</span>
+            <span className='m'>F</span>
+            <span className='m'>O</span>
+            <span className='m'>R</span>
+            <span className='m'>&nbsp;</span>
+            <span className='m'>S</span>
+            <span className='m'>O</span>
+            <span className='m'>R</span>
+            <span className='m'>C</span>
+            <span className='m'>E</span>
+            <span className='m'>&nbsp;</span>
+            <span className='m'>C</span>
+            <span className='m'>O</span>
+            <span className='m'>N</span>
+            <span className='m'>T</span>
+            <span className='m'>R</span>
+            <span className='m'>O</span>
+            <span className='m'>L</span>
         </h3>
-        <i id="loading-message" class="loading-message">Loading <span id="loading-task">configurations</span>, please wait...</i>
-        <i class="user-welcome">Welcome, <span id="username">nameless patron</span>!</i>
+        <i id="loading-message" className="loading-message">Loading <span id="loading-task">configurations</span>, please wait...</i>
+        <i className="user-welcome">Welcome, <span id="username">nameless patron</span>!</i>
     </div>;
 };
 
-ReactDOM.render(<loader />, document.getElementById('loader'));
+ReactDOM.render(<Loader />, document.getElementById('loader'));
