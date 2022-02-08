7c7ba4640fd9a8704f52a8ebb4de3ebbbeb28b71
tacosontitan <jkdavis1691@gmail.com>
2022-02-08 11:07:01 -0600

    Updated webpack config to support react as the renderer

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,41 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = [
     {
-      mode: 'development',
-      entry: './app/main.ts',
-      target: 'electron-main',
-      module: {
-        rules: [{
-          test: /\.ts$/,
-          include: /src/,
-          use: [{ loader: 'ts-loader' }]
-        }]
-      },
-      output: {
-        path: __dirname + '/src',
-        filename: 'main.js'
-      }
+        mode: 'development',
+        entry: './app/main.ts',
+        target: 'electron-main',
+        module: {
+            rules: [{
+                test: /\.ts$/,
+                include: /src/,
+                use: [{ loader: 'ts-loader' }]
+            }]
+        },
+        output: {
+            path: __dirname + '/dist',
+            filename: 'main.js'
+        }
+    },
+    {
+        mode: 'development',
+        entry: './app/react.tsx',
+        target: 'electron-renderer',
+        devtool: 'source-map',
+        module: {
+            rules: [{
+                test: /\.ts(x?)$/,
+                include: /src/,
+                use: [{ loader: 'ts-loader' }]
+            }]
+        },
+        output: {
+            path: __dirname + '/dist',
+            filename: 'react.js'
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './app/pages/index.html'
+            })
+        ]
     }
-  ];
+];
