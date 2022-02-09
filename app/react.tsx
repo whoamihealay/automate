5f879b4698cc357546c00ac87f1098812b318b88
tacosontitan <jkdavis1691@gmail.com>
2022-02-08 20:39:49 -0600

    Restructured loader to support cleaner implementation. Replaced hard coded colors with variables and classes.

--- app/react.tsx
+++ app/react.tsx
@@ -6,14 +6,13 @@ const Loader = () => {
     let messageElements = [];
     for (let i = 0; i < message.length; i++) {
         let character = message.charAt(i);
-        messageElements.push(<span className="m">{character}</span>)
+        messageElements.push(<span>{character}</span>)
     }
 
     return <div>
-        <h1>AUTOMATE</h1>
+        <h1 className="text-corn">AUTOMATE</h1>
         <h3 className="span loader-message">{messageElements}</h3>
         <i id="loading-message" className="loading-message">Loading <span id="loading-task">configurations</span>, please wait...</i>
-        <i className="user-welcome">Welcome, <span id="username">nameless patron</span>!</i>
     </div>;
 }
 ReactDOM.render(<Loader />, document.getElementById('loader'));
